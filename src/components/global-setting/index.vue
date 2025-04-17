ad851dd2a34989b9293d9e17637dfbf23c62dc23
EUForest <EUForest@tutamail.com>
2025-04-17 11:43:56 +0800

    remove: Page management area buttons

--- src/components/global-setting/index.vue
+++ src/components/global-setting/index.vue
@@ -17,7 +17,6 @@
   >
     <template #title> {{ $t('system.title') }} </template>
     <Block :options="contentOpts" :title="$t('settings.content')" />
-    <Block :options="othersOpts" :title="$t('settings.otherSettings')" />
   </a-drawer>
 </template>
 
@@ -49,24 +48,6 @@
     },
     { name: 'settings.footer', key: 'footer', defaultVal: appStore.footer },
     { name: 'settings.tabBar', key: 'tabBar', defaultVal: appStore.tabBar },
-    {
-      name: 'settings.menuFromServer',
-      key: 'menuFromServer',
-      defaultVal: appStore.menuFromServer,
-    },
-    {
-      name: 'settings.menuWidth',
-      key: 'menuWidth',
-      defaultVal: appStore.menuWidth,
-      type: 'number',
-    },
-  ]);
-  const othersOpts = computed(() => [
-    {
-      name: 'settings.colorWeak',
-      key: 'colorWeak',
-      defaultVal: appStore.colorWeak,
-    },
   ]);
 
   const cancel = () => {
